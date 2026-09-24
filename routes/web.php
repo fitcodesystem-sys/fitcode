2164044acf12a8a58101f549fe3fea751ca5ef91
fitcode <fitcode.system@gmail.com>
2026-09-24 20:05:18 -0300

    criando a rota e view para o cadastro de aulas

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+route::get('/aulas', function (){
+    return view('CadastroAula');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
